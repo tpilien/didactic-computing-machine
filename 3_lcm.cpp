ae451fa315ff57885a9753a9d8c1b2413b2e88d6
Tim Pilien <z5014873@zmail.unsw.edu.au>
2016-06-09 22:05:35 +1000

    added read preamble for lcm

---
+++ 3_lcm.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  unsigned int tt = 0;
+  cin >> tt;
+
+  for (unsigned int z = 0; z < tt; ++z) {
+    unsigned int cc = 0;
+    cin >> cc;
+
+    for (unsigned int i = 0; y < cc; ++a) {
+      unsigned int pp, a;
+      cin >> pp, a;
+    }
+  }
+
+  return 0;
+}
