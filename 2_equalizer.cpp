49f7cd43106e7fba23c126c9c53f16ba06ebd055
Tim Pilien <z5014873@zmail.unsw.edu.au>
2016-06-09 22:02:19 +1000

    added read preamble for equalizer

---
+++ 2_equalizer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  unsigned int cc = 0;
+  cin >> cc;
+
+  for (unsigned int z = 0; t < cc; ++z) {
+    string ss, tt;
+    cin ss >> tt;
+    // compare ss & tt here  
+  }
+
+  return 0;
+}
