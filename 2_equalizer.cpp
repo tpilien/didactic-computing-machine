7eb9d047c981dcd19677da9bdf71316969da7c42
Tim Pilien <z5014873@zmail.unsw.edu.au>
2016-06-12 16:31:24 +1000

    wrote bits_equalizer 3n time, editied readme

--- 2_equalizer.cpp
+++ 2_equalizer.cpp
@@ -6,10 +6,68 @@ int main() {
   unsigned int cc = 0;
   cin >> cc;
 
-  for (unsigned int z = 0; t < cc; ++z) {
+  for (unsigned int z = 1; z <= cc; ++z) {
     string ss, tt;
-    cin ss >> tt;
-    // compare ss & tt here  
+    cin >> ss >> tt;
+    
+    unsigned int ones = 0;
+    unsigned int zeros = 0;
+    
+    unsigned int edit_ones = 0;
+    unsigned int edit_zeros = 0;
+    // find the amount of ones and zeros in the completed string
+    // find the amount of ones and zeros in non-completed string
+    for (unsigned int i = 0; i < tt.size(); ++i) {
+      if (tt[i] == '0') {
+        zeros++;
+      } else if (tt[i] == '1') {
+        ones++;
+      }
+      
+      if (ss[i] == '0') {
+        edit_zeros++;
+      } else if (ss[i] == '1') {
+        edit_ones++;
+      }
+    }
+    
+    // greedily place 1s in the old string
+    unsigned int edits = 0;
+    for (unsigned int i = 0; i < ss.size(); ++i) {
+      if (ss[i] == '?' && tt[i] == '1' && edit_ones < ones) {
+        edits++;
+        edit_ones++;
+        ss[i] = '1';
+      } else if (ss[i] == '?' && tt[i] == '1' && edit_ones == ones) {
+        edits++;
+        edit_zeros++;
+        ss[i] = '0';
+      }
+    }
+    
+    // now place 0s in the old string and find the amount of misplaced tiles
+    unsigned int misplaced = 0;
+    for (unsigned int i = 0; i < ss.size(); ++i) {
+      if (ss[i] == '?' && tt[i] == '0' && edit_zeros < zeros) {
+        edits++;
+        edit_zeros++;
+        ss[i] = '0';
+      } else if (ss[i] == '?' && tt[i] == '0' && edit_zeros == zeros) {
+        edits++;
+        edit_ones++;
+        ss[i] = '1';
+      }
+      
+      if (ss[i] != tt[i]) {
+        misplaced++;
+      }
+    }
+    
+    if (zeros != edit_zeros && ones != edit_ones) {
+      cout << "Case " << z << ": -1" << endl;
+    } else {
+      cout << "Case " << z << ": " << edits + (misplaced/2) << endl;
+    }
   }
 
   return 0;
--- README.md
+++ README.md
@@ -4,3 +4,16 @@ Europe - Southwestern (Regionals 2012)
 Timothy Pilien - z5014873
 Justin Keevers - z5015443
 Nevin Lazarus - z
+
+
+Status:
+1 : Incomplete
+2 : Incomplete
+3 : Incomplete
+4 : Incomplete
+5 : Incomplete
+6 : Incomplete 
+7 : Complete - Tested
+8 : Incomplete
+9 : Incomplete
+10: Incomplete
